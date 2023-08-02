79c7fcdd9eacfe6c9a45b6cb909d0e5129b6ee9e
KimSound424 <ykj1512@gmail.com>
2023-08-02 16:38:38 +0900

    [Branch] feature/profile 브랜치 생성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import StyledApp from '@styles/App-styled';
+import Profile from './pages';
 
 const App = () => {
   return (
     <StyledApp>
-      <p>Hello World</p>
+      <Profile />
     </StyledApp>
   );
 };
