ae06dbc956c2d44ea6713504e45da2741e7fec33
BangDori <qudwns8865@naver.com>
2023-08-04 18:36:38 +0900

    [Feat] 로그인 회원가입 라우팅 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import RootLayout from '@pages/RootLayout';
+import ErrorPage from '@pages/Error';
+import HomePage from '@pages/Home';
+import AuthenticationPage from '@pages/Authentication';
+import { loader as logoutLoader } from '@pages/Logout';
+
 import StyledApp from '@styles/App-styled';
+import { tokenLoader, checkTokenLoader, checkIsTokenLoader } from '@utils/auth';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    id: 'root',
+    loader: tokenLoader,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+        loader: checkTokenLoader,
+      },
+      {
+        path: 'auth',
+        element: <AuthenticationPage />,
+        loader: checkIsTokenLoader,
+      },
+      {
+        path: 'logout',
+        loader: logoutLoader,
+      },
+    ],
+  },
+]);
 
 const App = () => {
   return (
     <StyledApp>
-      <p>Hello World</p>
+      <RouterProvider router={router} />
     </StyledApp>
   );
 };
