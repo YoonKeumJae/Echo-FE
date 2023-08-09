3ba29fcb650c96f89b8dac5261b886f978fc4b9a
BangDori <qudwns8865@naver.com>
2023-08-09 18:33:03 +0900

    [Refactor] AccountForm 설명 및 네비게이션 분리

--- src/components/auth/Account/AccountForm.js
+++ src/components/auth/Account/AccountForm.js
@@ -1,50 +1,16 @@
-import { Link, NavLink, Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 
 import StyledSection from '@styles/auth/account/AccountForm-styled';
-// import SearchID from './SearchID';
-// import FindID from './FindID';
-// import SearchPWD from './SearchPWD';
-// import NewPassword from './NewPassword';
 
-const AccountForm = ({ mode }) => {
+const AccountForm = () => {
   return (
     <StyledSection>
       <div className='account-section'>
-        <Link to='/'>
+        <NavLink to='/'>
           <h2 className='logo'>Logo</h2>
-        </Link>
-        <p className='description'>
-          {mode === 'search' && '아이디/비밀번호 찾기'}
-          {mode === 'completed' && '아이디 찾기가 완료되었습니다.'}
-          {mode === 'new' && '새로운 비밀번호를 설정해주세요.'}
-        </p>
-
-        {mode === 'search' && (
-          <div className='account-mode'>
-            <NavLink
-              to='id?mode=search'
-              className={({ isActive }) => (isActive ? 'selected' : undefined)}
-            >
-              아이디 찾기
-            </NavLink>
-            <NavLink
-              to='password?mode=search'
-              className={({ isActive }) => (isActive ? 'selected' : undefined)}
-            >
-              비밀번호 찾기
-            </NavLink>
-          </div>
-        )}
+        </NavLink>
 
         <Outlet />
-
-        {mode === 'search' && (
-          <p className='navigation'>
-            <Link to='/auth/signup' className='navigation-signup'>
-              계정이 없으신가요?
-            </Link>
-          </p>
-        )}
       </div>
     </StyledSection>
   );
--- src/components/auth/Account/NewPassword.js
+++ src/components/auth/Account/NewPassword.js
@@ -34,64 +34,68 @@ const NewPassword = () => {
   };
 
   return (
-    <StyledDiv>
-      <div className='find-id'>
-        <p>아이디</p>
-        <p className='find'>jay0214</p>
-      </div>
-      <form className='newPassword-form' onSubmit={handleSubmit(onSubmit)}>
-        <div className='input-container'>
-          <label htmlFor='newPassword'>새 비밀번호</label>
-          <input
-            id='newPassword'
-            type='password'
-            placeholder='비밀번호 입력 (문자, 숫자, 특수문자 포함 8~16자)'
-            {...register('newPassword', {
-              required: '※ 필수 항목 입니다.',
-              pattern: {
-                value: regExpPassword,
-                message:
-                  '※ 문자, 숫자, 특수문자를 포함한 16자 이내의 비밀번호를 입력해주세요.',
-              },
-            })}
-          />
-          <div className='input-validation'>
-            <ErrorMessage errors={errors} name='newPassword' />
-          </div>
-        </div>
-        <div className='input-container'>
-          <label htmlFor='confirmNewPassword' />
-          <input
-            id='confirmNewPassword'
-            type='password'
-            placeholder='비밀번호 재입력'
-            {...register('confirmNewPassword', {
-              required: '※ 필수 항목 입니다.',
-              validate: (val) => {
-                if (watch('newPassword') !== val) {
-                  return '※ 비밀번호가 일치하지 않습니다.';
-                }
+    <>
+      <p className='description'>새로운 비밀번호를 설정해주세요.</p>
 
-                return null;
-              },
-            })}
-          />
-          <div className='input-validation'>
-            <ErrorMessage errors={errors} name='confirmNewPassword' />
+      <StyledDiv>
+        <div className='find-id'>
+          <p>아이디</p>
+          <p className='find'>jay0214</p>
+        </div>
+        <form className='newPassword-form' onSubmit={handleSubmit(onSubmit)}>
+          <div className='input-container'>
+            <label htmlFor='newPassword'>새 비밀번호</label>
+            <input
+              id='newPassword'
+              type='password'
+              placeholder='비밀번호 입력 (문자, 숫자, 특수문자 포함 8~16자)'
+              {...register('newPassword', {
+                required: '※ 필수 항목 입니다.',
+                pattern: {
+                  value: regExpPassword,
+                  message:
+                    '※ 문자, 숫자, 특수문자를 포함한 16자 이내의 비밀번호를 입력해주세요.',
+                },
+              })}
+            />
+            <div className='input-validation'>
+              <ErrorMessage errors={errors} name='newPassword' />
+            </div>
           </div>
-        </div>
+          <div className='input-container'>
+            <label htmlFor='confirmNewPassword' />
+            <input
+              id='confirmNewPassword'
+              type='password'
+              placeholder='비밀번호 재입력'
+              {...register('confirmNewPassword', {
+                required: '※ 필수 항목 입니다.',
+                validate: (val) => {
+                  if (watch('newPassword') !== val) {
+                    return '※ 비밀번호가 일치하지 않습니다.';
+                  }
 
-        <div className='button-container'>
-          <button
-            type='submit'
-            className='login-button'
-            disabled={isSubmitting}
-          >
-            확인
-          </button>
-        </div>
-      </form>
-    </StyledDiv>
+                  return null;
+                },
+              })}
+            />
+            <div className='input-validation'>
+              <ErrorMessage errors={errors} name='confirmNewPassword' />
+            </div>
+          </div>
+
+          <div className='button-container'>
+            <button
+              type='submit'
+              className='login-button'
+              disabled={isSubmitting}
+            >
+              확인
+            </button>
+          </div>
+        </form>
+      </StyledDiv>
+    </>
   );
 };
 
