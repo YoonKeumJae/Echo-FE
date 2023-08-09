1ff626a16260d5a8b06240157889474e916c9697
BangDori <qudwns8865@naver.com>
2023-08-09 18:35:30 +0900

    [Design] 아이디 찾기 UI 수정

--- src/styles/auth/account/FindID-styled.js
+++ src/styles/auth/account/FindID-styled.js
@@ -22,18 +22,18 @@ const StyledDiv = styled.div`
     }
   }
 
-  .button-container {
+  .login-button {
     margin-top: 64px;
 
-    .login-button {
-      background: #243dc5;
-      flex: 1;
-      border-radius: 5px;
-      height: 32px;
-      font-weight: 600;
-      color: white;
+    background: #243dc5;
+    border-radius: 5px;
+    height: 32px;
+    font-weight: 600;
 
-      cursor: pointer;
+    cursor: pointer;
+
+    & a {
+      color: white;
     }
   }
 `;
