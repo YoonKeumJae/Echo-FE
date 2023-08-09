2382b3f69d5dbb1f69b3cd3f64a1c87853fe6b77
BangDori <qudwns8865@naver.com>
2023-08-09 18:41:30 +0900

    [Design] 새로운 비밀번호 설정 아이디란 UI 제거

--- src/components/auth/Account/NewPassword.js
+++ src/components/auth/Account/NewPassword.js
@@ -5,7 +5,7 @@ import { ErrorMessage } from '@hookform/error-message';
 
 import { regExpPassword } from '@constants/regular-expression';
 import usePreventLeave from '@hooks/usePreventLeave';
-import StyledDiv from '@styles/auth/account/NewPassword-styled';
+import StyledForm from '@styles/auth/account/NewPassword-styled';
 
 const NewPassword = () => {
   const [enablePrevent, disablePrevent] = usePreventLeave();
@@ -17,9 +17,6 @@ const NewPassword = () => {
     watch,
   } = useForm({ mode: 'onBlur' });
   const navigate = useNavigate();
-
-  // eslint-disable-next-line no-console
-  console.log(errors);
 
   useEffect(() => {
     if (isDirty) enablePrevent();
@@ -37,64 +34,58 @@ const NewPassword = () => {
     <>
       <p className='description'>새로운 비밀번호를 설정해주세요.</p>
 
-      <StyledDiv>
-        <div className='find-id'>
-          <p>아이디</p>
-          <p className='find'>jay0214</p>
+      <StyledForm onSubmit={handleSubmit(onSubmit)}>
+        <div className='input-container'>
+          <label htmlFor='newPassword'>새 비밀번호</label>
+          <input
+            id='newPassword'
+            type='password'
+            placeholder='비밀번호 입력 (문자, 숫자, 특수문자 포함 8~16자)'
+            {...register('newPassword', {
+              required: '※ 필수 항목 입니다.',
+              pattern: {
+                value: regExpPassword,
+                message:
+                  '※ 문자, 숫자, 특수문자를 포함한 16자 이내의 비밀번호를 입력해주세요.',
+              },
+            })}
+          />
+          <div className='input-validation'>
+            <ErrorMessage errors={errors} name='newPassword' />
+          </div>
         </div>
-        <form className='newPassword-form' onSubmit={handleSubmit(onSubmit)}>
-          <div className='input-container'>
-            <label htmlFor='newPassword'>새 비밀번호</label>
-            <input
-              id='newPassword'
-              type='password'
-              placeholder='비밀번호 입력 (문자, 숫자, 특수문자 포함 8~16자)'
-              {...register('newPassword', {
-                required: '※ 필수 항목 입니다.',
-                pattern: {
-                  value: regExpPassword,
-                  message:
-                    '※ 문자, 숫자, 특수문자를 포함한 16자 이내의 비밀번호를 입력해주세요.',
-                },
-              })}
-            />
-            <div className='input-validation'>
-              <ErrorMessage errors={errors} name='newPassword' />
-            </div>
-          </div>
-          <div className='input-container'>
-            <label htmlFor='confirmNewPassword' />
-            <input
-              id='confirmNewPassword'
-              type='password'
-              placeholder='비밀번호 재입력'
-              {...register('confirmNewPassword', {
-                required: '※ 필수 항목 입니다.',
-                validate: (val) => {
-                  if (watch('newPassword') !== val) {
-                    return '※ 비밀번호가 일치하지 않습니다.';
-                  }
+        <div className='input-container'>
+          <label htmlFor='confirmNewPassword' />
+          <input
+            id='confirmNewPassword'
+            type='password'
+            placeholder='비밀번호 재입력'
+            {...register('confirmNewPassword', {
+              required: '※ 필수 항목 입니다.',
+              validate: (val) => {
+                if (watch('newPassword') !== val) {
+                  return '※ 비밀번호가 일치하지 않습니다.';
+                }
 
-                  return null;
-                },
-              })}
-            />
-            <div className='input-validation'>
-              <ErrorMessage errors={errors} name='confirmNewPassword' />
-            </div>
+                return null;
+              },
+            })}
+          />
+          <div className='input-validation'>
+            <ErrorMessage errors={errors} name='confirmNewPassword' />
           </div>
+        </div>
 
-          <div className='button-container'>
-            <button
-              type='submit'
-              className='login-button'
-              disabled={isSubmitting}
-            >
-              확인
-            </button>
-          </div>
-        </form>
-      </StyledDiv>
+        <div className='button-container'>
+          <button
+            type='submit'
+            className='login-button'
+            disabled={isSubmitting}
+          >
+            확인
+          </button>
+        </div>
+      </StyledForm>
     </>
   );
 };
