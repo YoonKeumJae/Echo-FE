4302755249d45aafc4e21368e6d89fc1fbd43cf8
BangDori <qudwns8865@naver.com>
2023-08-11 22:13:21 +0900

    [Feat] 게시물과 댓글 컴포넌트 분리

--- src/pages/home/PostDetail.js
+++ src/pages/home/PostDetail.js
@@ -1,16 +1,55 @@
+import { Suspense } from 'react';
 import { json, defer, useLoaderData, Await } from 'react-router-dom';
 
 import { getPost } from '@services/post';
-import Post from '@components/home/Post';
-import { Suspense } from 'react';
+import PostItem from '@components/home/post/PostItem';
+import CommentList from '@components/home/post/CommentList';
+
+const DUMMY_COMMENTS = [
+  {
+    id: 1,
+    profile: '',
+    username: '이동혁',
+    date: '1분전',
+    content: 'dd',
+    commentCount: '0',
+    like: '3',
+    isLike: false,
+  },
+  {
+    id: 2,
+    profile: '',
+    username: '이동혁',
+    date: '3분전',
+    content: 'ㅎㅇㅁㄴㅀㅁㄴㅇㅎㅇㄴㅁㅎㄴㅇㅁ',
+    commentCount: '0',
+    like: '1',
+    isLike: true,
+  },
+  {
+    id: 3,
+    profile: '',
+    username: '이동혁',
+    date: '5분전',
+    content: 'gasgasdgsadgsadgsdangldss',
+    commentCount: '0',
+    like: '1',
+    isLike: false,
+  },
+];
 
 const PostDetailPage = () => {
   const { post } = useLoaderData();
 
   return (
-    <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
-      <Await resolve={post}>{(loadedPost) => <Post post={loadedPost} />}</Await>
-    </Suspense>
+    <>
+      <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+        <Await resolve={post}>
+          {(loadedPost) => <PostItem post={loadedPost} />}
+        </Await>
+      </Suspense>
+      <CommentList comments={DUMMY_COMMENTS} />
+    </>
   );
 };
 
