94e6d68b8666b0ec6066781032f76b25511ee6aa
BangDori <qudwns8865@naver.com>
2023-08-15 16:47:13 +0900

    [Feat] 게시글 작성시 개행문자 추가 기능

--- src/constants/regular-expression.js
+++ src/constants/regular-expression.js
@@ -6,5 +6,13 @@ const regExpName = /^[가-힣]+$/;
 const regExpNickname = /^(?=.*[a-zA-Z0-9가-힣])[a-zA-Z0-9가-힣\d`~_]{2,8}$/;
 const regExpPhone =
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/;
+const regLineBreak = /\r\n|\r|\n/g; // 개행 문자 패턴 정규식
 
-export { regExpID, regExpPassword, regExpName, regExpNickname, regExpPhone };
+export {
+  regExpID,
+  regExpPassword,
+  regExpName,
+  regExpNickname,
+  regExpPhone,
+  regLineBreak,
+};
