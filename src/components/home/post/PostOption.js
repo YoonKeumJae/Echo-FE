e57c3c1976c49e269b37deb2d3ae39db48b497ca
BangDori <qudwns8865@naver.com>
2023-08-16 20:57:22 +0900

    [Fix] 게시물 삭제 버그 수정

--- src/components/home/post/PostOption.js
+++ src/components/home/post/PostOption.js
@@ -1,14 +1,14 @@
 import { useSubmit } from 'react-router-dom';
 import OptionBox from '@styles/home/post/PostOption-styled';
 
-const PostOption = ({ postId, onUpdate }) => {
+const PostOption = ({ postId, onUpdate, userId }) => {
   const submit = useSubmit();
 
   const onRemovePost = () => {
     const isRemove = window.confirm('게시물을 삭제하시겠습니까?');
 
     if (isRemove) {
-      submit({ postId }, { method: 'delete' });
+      submit({ postId, userId }, { method: 'DELETE' });
       alert('게시물이 정상적으로 삭제되었습니다.');
     }
   };
