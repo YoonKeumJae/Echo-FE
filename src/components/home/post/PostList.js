874f891bd0fd9de28404c799a4b416c27348fece
BangDori <qudwns8865@naver.com>
2023-08-16 18:05:27 +0900

    [Feat] 게시물 무한 스크롤 기능 구현

--- src/components/home/post/PostList.js
+++ src/components/home/post/PostList.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+
 import StyledDiv from '@styles/home/post/PostList-styled';
 import PostItem from './PostItem';
 
 const PostList = ({ posts }) => {
+  const [items, setItems] = useState(posts.slice(0, 30));
+  const [currentSize, setCurrentSize] = useState(60);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchData = useCallback(async () => {
+    setIsLoading(true);
+
+    setItems((prevItems) => [
+      ...prevItems,
+      ...posts.slice(currentSize, currentSize + 30),
+    ]);
+    setCurrentSize((prevSize) => prevSize + 30);
+  }, [currentSize, posts]);
+
+  const handleScroll = useCallback(() => {
+    if (
+      document.documentElement.scrollTop +
+        document.documentElement.clientHeight <=
+      document.documentElement.scrollHeight - 500
+    ) {
+      return null;
+    }
+    fetchData();
+  }, [fetchData]);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [handleScroll, isLoading]);
+
   return (
     <StyledDiv>
-      {posts.map((post) => (
+      {items.map((post) => (
         <React.Fragment key={post.id}>
           <PostItem post={post} />
           <div style={{ borderBottom: '1px solid #ccc' }} />
