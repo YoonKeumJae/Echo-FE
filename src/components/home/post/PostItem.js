07784e7e009f723c4c3f9745b8ccb4d61044f7fb
BangDori <qudwns8865@naver.com>
2023-08-18 17:40:29 +0900

    [Feat] 게시물 좋아요 기능 추가

--- src/components/home/post/PostItem.js
+++ src/components/home/post/PostItem.js
@@ -1,10 +1,11 @@
+import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 import profileIcon from '@assets/default/profileIcon.png';
 import messageIcon from '@assets/post/messageIcon.png';
-import likeIcon from '@assets/post/likeIcon.png';
 import shareIcon from '@assets/post/shareIcon.png';
+import { likePost } from '@services/post';
 import StyledPost from '@styles/home/post/Post-styled';
 import { formatDate } from '@utils/date';
 import PostOption from './PostOption';
@@ -18,12 +19,24 @@ const PostItem = ({ post }) => {
     comment_count: commentCount,
     content,
     updated_at: updatedAt,
-    likes,
     user_id: userId,
+    liked_users: likedUsers,
   } = post;
   const { id: currentId } = useSelector((state) => state.user);
+  const [isClickLike, setIsClickLike] = useState(false);
+  const [clickCount, setClickCount] = useState(0);
 
   const date = formatDate(updatedAt);
+
+  useEffect(() => {
+    const isLike = likedUsers
+      ? Object.values(likedUsers).includes(currentId)
+      : false;
+    const likes = likedUsers ? Object.values(likedUsers).length : 0;
+
+    setIsClickLike(isLike);
+    setClickCount(likes);
+  }, [likedUsers, currentId]);
 
   const isLink = pathname === '/' || pathname.includes('profile');
   const isMinePost = userId === currentId && pathname.includes('profile');
@@ -40,6 +53,30 @@ const PostItem = ({ post }) => {
   });
 
   const onClickUpdate = () => navigate(`/${id}/update`, { state: post });
+  const onClickLikePost = async () => {
+    if (isClickLike) {
+      // eslint-disable-next-line no-restricted-syntax
+      for (const key in likedUsers) {
+        if (likedUsers[key] === currentId) {
+          delete likedUsers[key];
+          break; // Once found and deleted, exit the loop
+        }
+      }
+
+      setIsClickLike(false);
+      setClickCount((prev) => prev - 1);
+      await likePost(id, likedUsers || {});
+      return;
+    }
+
+    const addLikedUsers =
+      clickCount > 0 ? (likedUsers[currentId] = currentId) : { id: currentId };
+
+    setIsClickLike(true);
+    setClickCount((prev) => prev + 1);
+    await likePost(id, addLikedUsers);
+  };
+
   const onCopyToClipBoard = async () => {
     try {
       await navigator.clipboard.writeText(window.location.href + id);
@@ -80,9 +117,25 @@ const PostItem = ({ post }) => {
           <img src={messageIcon} alt='message icon' />
           <span>{commentCount}</span>
         </button>
-        <button className='item'>
-          <img src={likeIcon} alt='like icon' />
-          <span>{likes}</span>
+        <button className='item' onClick={onClickLikePost}>
+          {/* {isLike && <img src={clickedLikeIcon} alt='clicked like icon' />}
+          {!isLike && <img src={likeIcon} alt='like icon' />} */}
+          <svg
+            width='24'
+            height='24'
+            viewBox='0 0 50 50'
+            fill={isClickLike ? 'red' : 'none'}
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path
+              d='M15.625 8.33325C9.29688 8.33325 4.16667 13.4635 4.16667 19.7916C4.16667 31.2499 17.7083 41.6666 25 44.0895C32.2917 41.6666 45.8333 31.2499 45.8333 19.7916C45.8333 13.4635 40.7031 8.33325 34.375 8.33325C30.5 8.33325 27.0729 10.2572 25 13.202C23.9434 11.697 22.5398 10.4688 20.9079 9.62126C19.2759 8.77376 17.4639 8.33195 15.625 8.33325Z'
+              stroke='black'
+              strokeWidth='1.5'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+            />
+          </svg>
+          <span>{clickCount}</span>
         </button>
         <button className='item' onClick={onCopyToClipBoard}>
           <img src={shareIcon} alt='share icon' />
